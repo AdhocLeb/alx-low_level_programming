069d7cf7025c776c1b02efa40e4eeb5ca0f6d00b
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-02-16 14:36:40 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
 #include<stdio.h>
-
 /**
-
- * main - Program using the printf function to print text
-
+ * main - Program that uses the printf function to print text
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
-
 printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
-return (0);}

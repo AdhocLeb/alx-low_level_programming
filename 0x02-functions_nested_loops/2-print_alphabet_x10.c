fcd294fb848f9a797d03fcca12a3795709dedd95
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-02-21 13:53:39 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints  alphabets x10 times
- * Return: void
- */
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercases
+*/
 
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
+	int count = 0;
+	char letter;
 
-	char c;
-	int i = 0;
-
-	while (i <= 9)
+	while (count++ <= 9)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }

d9ed12e43bb76d7cfe07f8b9dba6b728e9b8ee79
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-02-21 14:22:12 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,14 @@
+#include "main.h"
 
-#include "holberton.h"
 /**
- * _isalpha - Check Main input character
- * Description: function returns 1 if the character is a
- * letter, lowercase or uppercase.
- * Return: 1 or 0 in otherwise.
+ * _isalpha - Check if a character is an alphabet
+ * Return: 1 if charater is letter, lowercase or upper, o upper
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter = 1;
-		}
-	}
-	return (isletter);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
+

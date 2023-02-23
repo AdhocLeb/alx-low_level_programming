26bcb0ed0a88ffc15f78366edf03057b32395c24
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-02-23 12:27:31 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,13 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
 
-/* function to check if the given integer is positive or negative */
 void positive_or_negative(int i);
-
-/* function to find the largest number among three integers */
 int largest_number(int a, int b, int c);
-
-/* function to convert the given day and month into day of the year */
 int convert_day(int month, int day);
-
-/* function to print the remaining days in the year from the given date */
 void print_remaining_days(int month, int day, int year);
 
-#endif
 
+#endif /* MAIN_H */

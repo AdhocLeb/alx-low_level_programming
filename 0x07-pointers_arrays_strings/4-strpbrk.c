fde29877e97a82690e1b5c50e8a4b67988a3517b
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-03-06 13:17:12 -0800

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,13 @@
+#include "main.h"
+
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: pointer to the string to search
- * @accept: pointer to the set of bytes to match
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
- * Return: pointer to the byte in s that matches one of the bytes
- * in accept, or NULL if no such byte is found
+ * @s:first occurrence in the string
+ *
+ * @accept: matches one of the bytes, or @NULL if no such byte
+ *
+ * Return: a pointer to the byte
  */
 char *_strpbrk(char *s, char *accept)
 {

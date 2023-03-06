e3ee5fcb942c1b06691f1d7def098726245039d3
“Lebogang <Lebogang.Phalane10@gmail.com>
2023-03-06 12:54:53 -0800

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
+	int i;
+		while (*s)
 		{
-			if (s[i] == accept[j])
-			return (&s[i]);
+			for (i = 0; accept[i]; i++)
+			{
+				if (*s == accept[i])
+					return (s);
+			}
+			s++;
 		}
-	}
-
-return (NULL);
+	return (NULL);
 }

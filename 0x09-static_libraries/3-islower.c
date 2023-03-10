08f52bbb077bc6c2e8723e148ef8f3594282001b
AdhocLeb <122960792+AdhocLeb@users.noreply.github.com>
2023-03-11 00:10:05 +0200

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower - checksfor lowercase character
- * @c: the integer value it receives
- * Return: 1 if charcacter c is lowercase,
+ * _islower - checks for lowercase character
+ * @c: The integer value it receives
+ * Return: 1 if charcacter c is lowercase.
  * 0 if false
  */
 int _islower(int c)
